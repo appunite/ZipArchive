36467a2876cc004d4dabda6b8b7cfb1ee313f8ee
Piotr Adamczak <piotr.adamczak@appunite.com>
2012-09-10 18:29:47 +0200

    Static Library

---
+++ ZipArchive/libZipArchive/libZipArchive.h
@@ -0,0 +1,13 @@
+//
+//  libZipArchive.h
+//  libZipArchive
+//
+//  Created by Emil Wojtaszek on 25.01.2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface libZipArchive : NSObject
+
+@end
---
+++ ZipArchive/libZipArchive/libZipArchive.m
@@ -0,0 +1,23 @@
+//
+//  libZipArchive.m
+//  libZipArchive
+//
+//  Created by Emil Wojtaszek on 25.01.2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "libZipArchive.h"
+
+@implementation libZipArchive
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
